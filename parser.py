0b8e2bebb17dd49a587880642d9895bc44850071
Enotii <anesnovpi21v@gmail.com>
2025-11-26 16:05:10 +0300

    Исправление URL из-за ошибки 498

--- parser.py
+++ parser.py
@@ -5,18 +5,18 @@ proxies = os.getenv('PROXIES')
 
 
 def get_query():
-    url = 'https://www.wildberries.ru/__internal/search/exactmatch/ru/common/v18/search?ab_testing=false&ab_testing=false&appType=1&autoselectFilters=false&curr=rub&dest=123585839&hide_dtype=11&inheritFilters=false&lang=ru&query=%D0%BF%D0%B0%D0%BB%D1%8C%D1%82%D0%BE%20%D0%B8%D0%B7%20%D0%BD%D0%B0%D1%82%D1%83%D1%80%D0%B0%D0%BB%D1%8C%D0%BD%D0%BE%D0%B9%20%D1%88%D0%B5%D1%80%D1%81%D1%82%D0%B8&resultset=filters&spp=30&suppressSpellcheck=false&uclusters=9'
+    url = 'https://search.wb.ru/exactmatch/ru/common/v9/search?ab_testing=false&appType=1&curr=rub&dest=-1257786&hide_dtype=13&lang=ru&page=1&query=%D0%BA%D1%80%D0%BE%D1%81%D1%81%D0%BE%D0%B2%D0%BA%D0%B8%20%D0%B6%D0%B5%D0%BD%D1%81%D0%BA%D0%B8%D0%B5&resultset=catalog&sort=popular&spp=30& suppressSpellcheck=false'
     headers = {
         'Accept': '*/*',
         'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3',
         'Connection': 'keep-alive',
         'DNT': '1',
         'Origin': 'https://www.wildberries.ru',
-        'Referer': 'https://www.wildberries.ru/catalog/0/search.aspx?search=%D0%BF%D0%B0%D0%BB%D1%8C%D1%82%D0%BE+%D0%B8%D0%B7+%D0%BD%D0%B0%D1%82%D1%83%D1%80%D0%B0%D0%BB%D1%8C%D0%BD%D0%BE%D0%B9+%D1%88%D0%B5%D1%80%D1%81%D1%82%D0%B8',
+        'Referer': 'https://www.wildberries.ru/catalog/0/search.aspx?search=%D0%BF%D0%B0%D0%BB%D1%8C%D1%82%D0%BE%20%D0%B8%D0%B7%20%D0%BD%D0%B0%D1%82%D1%83%D1%80%D0%B0%D0%BB%D1%8C%D0%BD%D0%BE%D0%B9%20%D1%88%D0%B5%D1%80%D1%81%D1%82%D0%B8',
         'Sec-Fetch-Dest': 'empty',
         'Sec-Fetch-Mode': 'cors',
         'Sec-Fetch-Site': 'cross-site',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:144.0) Gecko/20100101 Firefox/144.0',
         'sec-ch-ua': '"Not)A;Brand";v="99", "Google Chrome";v="127", "Chromium";v="127"',
         'sec-ch-ua-mobile': '?0',
         'sec-ch-ua-platform': '"Windows"',
@@ -24,3 +24,32 @@ def get_query():
     response = requests.get(url=url, headers=headers, proxies=proxies)
 
     return response.json()
+
+def get_products(response):
+    products = []
+
+    products_raw = response.get('data', {}).get('products', None)
+
+    if products_raw != None and len(products_raw) > 0:
+        for product in products_raw:
+            products.append({
+                'brand': product.get('brand', None),
+                'name': product.get('name', None),
+                'id': product.get('id', None)
+            })
+
+    return products
+
+def check():
+    response = get_query()
+    products = get_products(response)
+
+    items = []
+
+    for product in products:
+        text = f"<b>Название</b>: {product['name']}\n<b>Бренд</b>: {product['brand']}"
+        items.append(text)
+
+    return items
+
+print(check())
