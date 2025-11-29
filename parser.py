bf01731f1bf92a3f1fca7d689a120e8e24ac5aa1
Enotii <anesnovpi21v@gmail.com>
2025-11-29 16:55:38 +0300

    Добавлена функция обработки через ThreadPool; функция скрапинга и парсинга товара перенесена в отдельную функцию

--- parser.py
+++ parser.py
@@ -1,4 +1,6 @@
+import time
 from ast import parse
+from concurrent.futures import ThreadPoolExecutor, as_completed
 from time import sleep
 from scrapper.iterator import PageIter
 from scrapper.pagescrapper import ProductScrapper
@@ -25,43 +27,86 @@ QUERY = '%D0%BF%D0%B0%D0%BB%D1%8C%D1%82%D0%BE%20%D0%B8%D0%B7%20%D0%BD%D0%B0%D1%8
 • Количество отзывов +
 """
 
+def get_parsed_product(article, pics):
+    parsed_product = None
+    fail_count = 5
+
+    while parsed_product is None and fail_count > 0:
+        try:
+            products_scrapper = ProductScrapper()
+
+            product = products_scrapper.get_product_info(article).get('products')
+            desc = products_scrapper.get_product_description(article)
+            if desc is not None:
+                product[0]['description'] = desc
+            product[0]['images'] = products_scrapper.get_product_images(article, pics)
+
+            parsed_product = parse_wb_json(product[0])
+            return parsed_product
+
+        except Exception as e:
+            print(e)
+            print(f"Товар {article} не обработался")
+            fail_count -= 1
+
+    return parsed_product
+
+
 # Получение всех товаров на странице
 def get_products(response):
+
     products = []
 
     products_raw = response.get('products', None)
-
-    products_scrapper = ProductScrapper()
 
     count = 1
 
     while products_raw is not None and len(products) < len(products_raw):
         for item in products_raw:
 
-            product = products_scrapper.get_product_info(item['id']).get('products')
-            desk = products_scrapper.get_product_description(item['id'])
-            product[0]['description'] = desk
-            product[0]['images'] = products_scrapper.get_product_images(item['id'], item['pics'])
-
-            parsed_product = parse_wb_json(product[0])
-
-            products.append(parsed_product)
+            products.append(get_parsed_product(item.get('id'), item.get('pics')))
 
             print(f'Обработано товаров: {count}')
             count += 1
 
     return products
 
+def get_products_concurrent(response, max_workers=10):
+
+    products = []
+
+    products_raw = response.get('products', None)
+
+    count = 1
+
+    with ThreadPoolExecutor(max_workers=max_workers) as executor:
+        futures = {executor.submit(get_parsed_product, item.get('id'), item.get('pics')): item for item in products_raw}
+
+        for future in as_completed(futures):
+            article = futures[future]
+            data = future.result()
+            if data is not None:
+                print(f"[OK {count}] {article}")
+                count += 1
+                products.append(data)
+            else:
+                print(f"[FAIL {count}] {article}")
+
+    return products
+
 #
-def parse_wb(query):
+def parse_wb(query, stopper = 100, concurrent = True):
     iterclass = PageIter(query)
     pageiter = iter(iterclass)
 
     writer = XSLSWriter("all", "filtered")
+
+    count = 1
+
     try:
-        while True:
-            count = 1
-            products = get_products(next(pageiter))
+        while True and stopper > 0:
+
+            products = get_products_concurrent(next(pageiter), 20)
             if len(products) == 0 or products is None:
                 break
 
@@ -72,11 +117,12 @@ def parse_wb(query):
                     writer.write_item_to_ws(product, "all")
 
             print(f'Страница {count} обработана')
+
+            stopper -= 1
             count += 1
-            sleep(1)
+
     except Exception as e:
         print(e)
-        print("Преждевременная остановка парсинга")
 
     writer.save_to_excel("all", "wb_all.xlsx")
     writer.save_to_excel("filtered", "wb_filtered.xlsx")
