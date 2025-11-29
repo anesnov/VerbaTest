bf4a777d0cabdf37d0449d382db8f4c598127bca
Enotii <anesnovpi21v@gmail.com>
2025-11-29 16:52:47 +0300

    Добавлен вариант поиска корзины через "известные" номера vol

--- scrapper/pagescrapper.py
+++ scrapper/pagescrapper.py
@@ -1,10 +1,27 @@
 import requests
 import os
 
-from requests.exceptions import SSLError
-
 
 # Получение разных данных по товару
+RANGES = [
+    (0, 143, '01'),
+    (144, 287, '02'),
+    (288, 431, '03'),
+    (432, 719, '04'),
+    (720, 1007, '05'),
+    (1008, 1061, '06'),
+    (1062, 1115, '07'),
+    (1116, 1169, '08'),
+    (1170, 1313, '09'),
+    (1314, 1601, '10'),
+    (1602, 1655, '11'),
+    (1656, 1919, '12'),
+    (1920, 2045, '13'),
+    (1920, 2089, '14'),
+    (1920, 2089, '15'),
+    (1920, 2405, '16'),
+    (1920, 2837, '17')
+]
 class ProductScrapper:
 
     # Инициализация класса (запись HTTP заголовка для запросов)
@@ -19,7 +36,7 @@ class ProductScrapper:
         with open("headers/cookie.txt") as f:
             cookie = f.read()
 
-        self.basket = 10
+        self.basket = 1
         self.url = ''
         self.headers = {
             'Accept': '*/*',
@@ -47,20 +64,48 @@ class ProductScrapper:
 
         return response.json()
 
+    # Нахождение корзины "эмпирическим путём"
+    def get_basket(self, vol):
+        for start, end, num in RANGES:
+            if start <= vol <= end:
+                return num
+        return 18
+
     # Получение развёрнутого описания
     def get_product_description(self, product_id):
         part = product_id // 1000
         vol = product_id // 100000
 
         # Нахождение нужной корзины с изображениями и описанием
-        basket = 10
+
+        # Вариант 1: через известные корзины
+
+        basket = self.get_basket(vol)
+        url = f'https://basket-{basket}.wbcontent.net/vol{vol}/part{part}/{product_id}/info/ru/card.json'
+        response = requests.get(url=url, headers=self.headers, proxies=self.proxies)
+        if response.status_code == 200:
+            self.basket = int(basket)
+            return response.json()
+
+        response = None
+        # Вариант 2: перебором с первой
+
+        basket = 1
         while basket <= 30:
-            url = f'https://basket-{basket}.wbcontent.net/vol{vol}/part{part}/{product_id}/info/ru/card.json'
-            basket += 1
+            if basket < 10:
+                host = '0' + str(basket)
+            else :
+                host = str(basket)
+            url = f'https://basket-{host}.wbcontent.net/vol{vol}/part{part}/{product_id}/info/ru/card.json'
             response = requests.get(url=url, headers=self.headers, proxies=self.proxies)
+
             if response.status_code == 200:
                 self.basket = basket
                 return response.json()
+            else:
+                basket += 1
+
+        return None
 
     # Формирование списка ссылок на изображения в карточке
     def get_product_images(self, product_id, pics):
@@ -82,8 +127,13 @@ class ProductScrapper:
         #         basket += 1
 
         count = 1
+        if self.basket < 10:
+            host = '0' + str(self.basket)
+        else:
+            host = str(self.basket)
         for count in range(count, pics+1):
-            url = f'https://basket-{self.basket}.wbcontent.net/vol{vol}/part{part}/{product_id}/images/big/{count}.webp'
+
+            url = f'https://basket-{host}.wbcontent.net/vol{vol}/part{part}/{product_id}/images/big/{count}.webp'
             images.append(url)
 
         return images
